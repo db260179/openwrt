3d2cb5b02fdd3f7c7c98225edc6ad3e52f958c25
David Bentham <db260179@gmail.com>
2025-11-13 08:34:21 +0000

    mediatek:yuncore_ax835 - Remove wo firmware as we are using our own custom mt76 repo
    Signed-off-by: David Bentham <db260179@gmail.com>

--- target/linux/mediatek/image/filogic.mk
+++ target/linux/mediatek/image/filogic.mk
@@ -1057,7 +1057,7 @@ define Device/yuncore_ax835
   KERNEL_INITRAMFS := kernel-bin | lzma | \
 	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
   IMAGE/sysupgrade.bin := append-kernel | pad-to 128k | append-rootfs | pad-rootfs | check-size | append-metadata
-  DEVICE_PACKAGES := kmod-mt7981-firmware mt7981-wo-firmware
+  DEVICE_PACKAGES := kmod-mt7981-firmware
 endef
 TARGET_DEVICES += yuncore_ax835
 
