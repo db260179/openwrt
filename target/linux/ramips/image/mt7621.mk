3bb168119da4534812706d1650ab7e8c782d7e5a
David Bentham <db260179@gmail.com>
2025-11-29 15:18:39 +0000

    ramips: Add support for ComFast CF-AC-101
    Specifications:
    SoC: MediaTek MT7621AT
    RAM: 256 MiB
    Flash: 32 MiB NOR (Winbond W25Q256FV)
    Ethernet: 5 x 1Gbs
    Console Port: 1x RJ45
    Button: 1 x "Reset" button
    LED: 1x green LED
    Power: IEC mains (built in PSU), ports are POE capable
    
    Signed-off-by: David Bentham <db260179@gmail.com>

--- target/linux/ramips/image/mt7621.mk
+++ target/linux/ramips/image/mt7621.mk
@@ -325,6 +325,20 @@ define Device/bolt_arion
 endef
 TARGET_DEVICES += bolt_arion
 
+define Device/comfast_cf-ac101
+  $(Device/dsa-migration)
+  IMAGE_SIZE := 15360k
+  DEVICE_VENDOR := COMFAST
+  DEVICE_MODEL := CF-AC101
+  DEVICE_PACKAGES := -uboot-envtools
+  KERNEL := kernel-bin | append-dtb | lzma | uImage lzma
+  IMAGES += factory.bin
+  IMAGE/sysupgrade.bin := append-kernel | append-rootfs | pad-rootfs | \
+	check-size | append-metadata
+  IMAGE/factory.bin := append-kernel | append-rootfs | pad-rootfs | check-size
+endef
+TARGET_DEVICES += comfast_cf-ac101
+
 define Device/cudy_wr1300
   $(Device/dsa-migration)
   IMAGE_SIZE := 15872k
