7cbc9558fcbd4fce4481e73d03da36eab7fa63d1
David Bentham <db260179@gmail.com>
2026-07-05 09:33:36 +0100

    mediatek: nft_offload patches from
    https://github.com/chbgdn/openwrt/commits/bridge-flow-offload/

--- target/linux/mediatek/filogic/target.mk
+++ target/linux/mediatek/filogic/target.mk
@@ -2,7 +2,9 @@ ARCH:=aarch64
 SUBTARGET:=filogic
 BOARDNAME:=Filogic 8x0 (MT798x)
 CPU_TYPE:=cortex-a53
-DEFAULT_PACKAGES += fitblk kmod-crypto-hw-safexcel wpad-mbedtls hostapd-utils uboot-envtools
+DEFAULT_PACKAGES += \
+	fitblk kmod-crypto-hw-safexcel kmod-nf-conntrack-bridge \
+	wpad-mbedtls hostapd-utils uboot-envtools
 KERNELNAME:=Image dtbs
 DEFAULT_PROFILE:=openwrt_one
 
